8a90c6d5d84f85dea519f1339fc944d8b44f658a
Bade Iriabho <ebade@yahoo.com>
2012-07-24 14:56:19 -0500

    move var declarations outside of for loop

--- s3dbfu.js
+++ s3dbfu.js
@@ -349,25 +349,22 @@ if(!String.hasOwnProperty('trim')) {
 			            var that = this;
 			            $.getJSON(this.action, function (result) {
 			                if (result && result.length) {
+			                	var tmp_str,tmp_str1,reg=/(http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?\^=%&amp;:\/~\+#]*[\w\-\@?\^=%&amp;\/~\+#])?/;
 								for(var i=0; i < result[0].message.done.length; i++) {
-									var tmp_str = result[0].message.done[i].value.replace(' ', '');
-									var reg = /(http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?\^=%&amp;:\/~\+#]*[\w\-\@?\^=%&amp;\/~\+#])?/;
-									var tmp_str1 = tmp_str.match(reg);
+									tmp_str = result[0].message.done[i].value.replace(' ', '');
+									tmp_str1 = tmp_str.match(reg);
 									if(tmp_str1 && tmp_str1.length > 0) {
 										result[0].message.done[i].download_url = tmp_str1[0];
 									} else {
 										result[0].message.done[i].download_url = '#';
 									}
-								}								
-								//console.log(result);
+								}
 			                    $(that).fileupload('option', 'done')
 			                        .call(that, null, {result: result[0].message.done});
 			                }
 			            });
 			        });
-
 				}
-				
 			}
 		},
 		showmodal:function(str) {
